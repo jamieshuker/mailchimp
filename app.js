95713f880343aad1ba3796aaf612d6e3b1197192
jamieshuker <jamieshuker@gmail.com>
2015-06-27 21:46:27 +0100

    config

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 3000); 
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || 127.0.0.1);
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
